742fc1b07fe6f3907078a5e5011176b7b8c90fa2
Panova Natalia <pannat.job@gmail.com>
2021-02-20 23:09:59 +0300

    Initial

---
+++ src/app/shared/cat-facts.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+
+type FactsRequestParams = {
+  ['animal_type']: string
+  amount: string
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatFactsService {
+
+  constructor(private http: HttpClient) { }
+
+  public getFact(id: string): Observable<any> {
+    return this.http.get(`${environment.endPoint}/facts/${id}`);
+  }
+
+  public getFacts(params: FactsRequestParams): Observable<any> {
+      return this.http.get(`${environment.endPoint}/facts/random`, { params });
+  }
+
+  public getUser(): Observable<any> {
+    return this.http.get(`${environment.endPoint}/users/me`);
+  }
+}
