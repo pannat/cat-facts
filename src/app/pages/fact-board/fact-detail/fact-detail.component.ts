503bf691cd1639fad578b3f2b9f34df8e610fe36
Panova Natalia <pannat.job@gmail.com>
2021-03-15 10:35:31 +0300

    learn async pipe

--- src/app/pages/fact-board/fact-detail/fact-detail.component.ts
+++ src/app/pages/fact-board/fact-detail/fact-detail.component.ts
@@ -1,49 +1,28 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CatFactsService } from '../../../shared/services/cat-facts.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Fact } from '../../../shared/types/Fact';
-import { User } from '../../../shared/types/User';
-import { Subscription } from 'rxjs';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CatFactsService} from '../../../shared/services/cat-facts.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Fact} from '../../../shared/types/Fact';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+import {User} from '../../../shared/types/User';
 
 @Component({
   selector: 'app-fact-detail',
   templateUrl: './fact-detail.component.html',
-  styleUrls: ['./fact-detail.component.scss']
+  styleUrls: ['./fact-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FactDetailComponent implements OnInit, OnDestroy {
-  public isLoading = false;
-  private id: string | undefined;
-  public fact: Fact<User> | undefined;
+export class FactDetailComponent {
   public format = 'dd/MM/yyyy hh:mm';
-  private subscriptions: Subscription = new Subscription;
+  readonly fact$: Observable<Fact<User>> = this.route.params.pipe(
+      switchMap((params) => this.catFactsService.getFact(params['id']))
+  );
 
-  constructor(private catFactsService: CatFactsService, private route: ActivatedRoute, private router: Router) {
-    const routeStream = this.route.params
-      .subscribe((result) => {
-        this.id = result.id;
-      })
-    this.subscriptions.add(routeStream);
-  }
-
-  ngOnInit(): void {
-    if (this.id) {
-      this.isLoading = true;
-
-      const factRequest = this.catFactsService.getFact(this.id)
-        .subscribe((result: Fact<User>) => {
-          this.fact = result;
-          this.isLoading = false;
-          console.log(result);
-        });
-      this.subscriptions.add(factRequest);
-    } else {
-      throw Error('Parameter Id is undefined');
-    }
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-  }
+  constructor(
+    private readonly catFactsService: CatFactsService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+  ) {}
 
   public onBack(): void {
     this.router.navigate(['/pages/board']);
